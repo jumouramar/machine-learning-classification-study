612040e75df3ee395d7d35e67119d7b4693d5bf9
jumouramar <juliana_moura@id.uff.br>
2025-01-12 02:28:04 -0300

    feat: add decision tree training

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Estudo em Python para treinar Árvores de Decisão, Random Forest e Rede Neural 
    
 2. train_test_split.py: K-Fold Cross-Validation escolhido como abordagem de divisão do dataset em treinamento e teste. Foi escolhido pois reduz a variação nos resultados já que testa o modelo em várias divisões diferentes de dados.
 
+3. decision_tree_classifier.py: treinamento de Árvore de Decisão
+
 ### Como executar o código?
 
 1. Prepare o ambiente
---
+++ decision_tree_classifier.py
@@ -0,0 +1,31 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.model_selection import KFold
+
+df = pd.read_csv("WineQT.csv", sep=",", encoding="utf-8")
+
+df_quality = df['quality']
+df = df.drop('quality', axis=1)
+
+kf = KFold(n_splits=4, shuffle=True, random_state=42)
+
+for fold, (train_index, test_index) in enumerate(kf.split(df), 1):
+    df_train, df_test = df.iloc[train_index], df.iloc[test_index]
+    df_quality_train, df_quality_test = df_quality.iloc[train_index], df_quality.iloc[test_index]
+    
+    # Cria o modelo
+    model = DecisionTreeClassifier(random_state=42)
+
+    # Treina
+    model.fit(df_train, df_quality_train)
+
+    # Predição
+    pred_test = model.predict(df_test)
+
+    # Avaliação
+    accuracy = accuracy_score(df_quality_test, pred_test)
+    f1 = f1_score(df_quality_test, pred_test, average='weighted')
+
+    print(f"Acurácia: {accuracy:.4f}")
+    print(f"F1 Score: {f1:.4f}")
+    print("-" * 50)
